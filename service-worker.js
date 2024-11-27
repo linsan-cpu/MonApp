ead12bcf4d0155283a5891b3e8d16ef79c6fd498
linsan-cpu <balhassane02@gmail.com>
2024-11-27 18:13:53 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,3 @@
-// service-worker.js
-
-/*self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('app-cache').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/styles.css',
-        '/script.js',
-        '/manifest.json',
-        '/C:\Users\PC\Desktop\MonApp/Marianna.jpg'
-      ]);
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-*/
-
 
 
 self.addEventListener('install', event => {
@@ -33,7 +8,7 @@ self.addEventListener('install', event => {
         '/styles.css',
         '/script.js',
         '/manifest.json',
-        '/Marianna' // Assurez-vous que ce fichier est présent
+        '/Marianna.jpg' // Assurez-vous que ce fichier est présent
       ]);
     })
   );
@@ -42,7 +17,30 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+      /*return response || fetch(event.request);*/
+      return response || fetch(event.request).then(networkResponse => {
+        // Sinon, il est récupéré du réseau
+        return caches.open('app-cache').then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+        });
+    });
+})
+);
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['app-cache'];
+  event.waitUntil(
+      caches.keys().then(cacheNames => {
+          return Promise.all(
+              cacheNames.map(cacheName => {
+                  if (!cacheWhitelist.includes(cacheName)) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
   );
 });
+
